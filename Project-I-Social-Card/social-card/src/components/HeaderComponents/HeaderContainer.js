48970c6a3af3eda5a08884f0c58deff463c7e7b9
JamesB <jblazer1@yahoo.com>
2019-02-27 12:45:51 -0800

    Construct main components and add image to ImageThumbnail component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+import ImageThumbnail from "./ImageThumbnail";
+
+function HeaderContainer() {
+  return (
+    <React.Fragment>
+      <ImageThumbnail />
+      {/* <HeaderTitle /> */}
+      {/* <HeaderContent /> */}
+    </React.Fragment>
+  );
+}
+
+export default HeaderContainer;
