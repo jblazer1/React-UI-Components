3abeeb19890e4b8f869ac926416bf08c9d733f4c
JamesB <jblazer1@yahoo.com>
2019-02-28 10:26:34 -0800

    CardContainer component styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Card.css";
 
 import CardBanner from "./CardBanner";
+import CardContent from "./CardContent";
 
 function CardContainer() {
   return (
     <div className="card-container">
       <a href="https://reactjs.org">
         <CardBanner />
+        <CardContent />
       </a>
     </div>
   );
