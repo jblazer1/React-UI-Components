daceb3639613c8509d9c4b45b718a3762f735d3b
JamesB <jblazer1@yahoo.com>
2019-02-27 19:32:32 -0800

    Finished HeaderContainer with borders and add React Atom logo

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="main-container">
       <HeaderContainer />
-      {/* <CardContainer /> */}
+      <CardContainer />
       {/* <Footer /> */}
     </div>
   );
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./Header.css";
 
 import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
 
 function HeaderContainer() {
   return (
-    <React.Fragment>
+    <div className="header-container">
       <ImageThumbnail />
-      {/* <HeaderTitle /> */}
-      {/* <HeaderContent /> */}
-    </React.Fragment>
+      <div className="header-content">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
   );
 }
 
